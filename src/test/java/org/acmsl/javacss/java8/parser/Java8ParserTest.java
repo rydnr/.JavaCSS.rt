7ba476de125db773f116c30658d1d49007da80dd
chous <chous@acm-sl.org>
2015-02-24 16:54:30 +0100

    201502241654

--- src/test/java/org/acmsl/javacss/java8/parser/Java8ParserTest.java
+++ src/test/java/org/acmsl/javacss/java8/parser/Java8ParserTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import org.antlr.v4.runtime.tree.ParseTree;
+
 @RunWith(JUnit4.class)
 public Java8ParserTest {
     @Test public void can_parse_an_interface_with_extends_and_a_single_method()
