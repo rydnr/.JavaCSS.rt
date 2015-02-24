a2c0788d398825282d3493a992d7272e85581d46
chous <chous@acm-sl.org>
2015-02-24 18:36:21 +0100

    201502241836

---
+++ src/main/java/org/acmsl/javacss/java8/parser/MethodHelper.java
@@ -0,0 +1,20 @@
+package org.acmsl.javacss.java8.parser;
+
+import java.text.MessageFormat;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class MethodHelper {
+    private final String input;
+
+    public MethodHelper(String input) {
+        this.input = input;
+    }
+
+    public int countMethods() {
+        return 0;
+    }
+}
