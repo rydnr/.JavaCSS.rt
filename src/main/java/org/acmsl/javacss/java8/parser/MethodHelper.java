316f9f1bb8049021b199ad58b1a6213eab8fc9a6
chous <chous@acm-sl.org>
2015-02-25 16:43:20 +0100

    201502251643

--- src/main/java/org/acmsl/javacss/java8/parser/MethodHelper.java
+++ src/main/java/org/acmsl/javacss/java8/parser/MethodHelper.java
@@ -35,7 +35,6 @@ public class MethodHelper {
             new Java8BaseListener() {
                 @Override
                 public void exitMethodDeclarator(@NotNull final MethodDeclaratorContext ctx) {
-                    result++;
                     super.exitMethodDeclarator(ctx);
                 }
             };
