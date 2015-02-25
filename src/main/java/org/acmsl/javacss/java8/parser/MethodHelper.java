44aa4510ae655954cca6bb5043ae60b06b206750
chous <chous@acm-sl.org>
2015-02-25 18:31:57 +0100

    201502251831

--- src/main/java/org/acmsl/javacss/java8/parser/MethodHelper.java
+++ src/main/java/org/acmsl/javacss/java8/parser/MethodHelper.java
@@ -86,7 +86,8 @@ public class MethodHelper {
 
         @Override
         public void exitMethodHeader(@NotNull final MethodDeclaratorContext ctx) {
-            returnTypes.add(ctx.getTokens(Java8Parser.RULE_result).get()
+            returnTypes.add(ctx.getTokens(Java8Parser.RULE_result).get(0);
+
             super.exitMethodDeclarator(ctx);
         }
 
