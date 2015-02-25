b0cdb0ff5f70a6f3b7ce793247f81a4a5e7d36dc
chous <chous@acm-sl.org>
2015-02-25 17:07:14 +0100

    201502251707

--- src/test/java/org/acmsl/javacss/java8/parser/MethodHelperTest.java
+++ src/test/java/org/acmsl/javacss/java8/parser/MethodHelperTest.java
@@ -47,7 +47,7 @@ public class MethodHelperTest {
         throws Exception {
 
         List<String> returnTypes;
-        String[] availableTypes = { "void", "String", "int", ""}
+        String[] availableTypes = { "void", "String", "int", "List<?>", "Map" }
 
         for (int i = 0; i < 10; i++) {
             retrieveReturnTypeOfMethodTest(i);
