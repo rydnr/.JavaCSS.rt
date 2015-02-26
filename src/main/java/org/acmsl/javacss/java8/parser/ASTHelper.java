fcbefe9da57a881a450778437b5c0466324d5cdb
chous <chous@acm-sl.org>
2015-02-26 16:31:41 +0100

    201502261631

--- src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
+++ src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
@@ -82,7 +82,6 @@ public class ASTHelper
         public Object visitCompilationUnit(@org.antlr.v4.runtime.misc.NotNull final CompilationUnitContext ctx)
         {
             List<ImportDeclarationContext> imports = ctx.importDeclaration();
-            ImportDeclarationContext sampleImport
             ImportDeclarationContext newImport = new ImportDeclarationContext(ctx, ctx.invokingState);
             newImport.addChild(
                 new CommonToken(Java8Parser.IMPORT)
