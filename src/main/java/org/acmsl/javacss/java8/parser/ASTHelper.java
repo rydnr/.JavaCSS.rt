b8720950bda41457ffc4f19a7423889cb218a03a
chous <chous@acm-sl.org>
2015-02-26 15:21:01 +0100

    201502261521

---
+++ src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
@@ -0,0 +1,65 @@
+/*
+                        JavaCSS
+
+    Copyright (C) 2015-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: jose.sanleandro@acm-sl.com
+
+ ******************************************************************************
+ *
+ * Filename: ASTHelper.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: 
+ *
+ * Date: 2015/02/26
+ * Time: 15:20
+ *
+ */
+package org.acmsl.javacss.java8.parser;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * Importing checkthread.org annotations.
+ */
+import org.checkthread.annotations.ThreadSafe;
+
+/**
+ *
+ * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
+ * @since 3.0
+ * Created: 2015/02/26 15:20
+ */
+@ThreadSafe
+public class ASTHelper
+{
+    private final ParseTree tree;
+
+    public ASTHelper(ParseTree ast) {
+        this.tree = ast;
+    }
+
+
+}
