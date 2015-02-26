d3dd8f6fab338255996f2544159a719a7b49dc6a
chous <chous@acm-sl.org>
2015-02-26 16:24:35 +0100

    201502261624

--- src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
+++ src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
@@ -81,6 +81,7 @@ public class ASTHelper
         public Object visitCompilationUnit(@org.antlr.v4.runtime.misc.NotNull final CompilationUnitContext ctx)
         {
             List<ImportDeclarationContext> imports = ctx.importDeclaration();
+
             TerminalNode leaf = new TerminalNodeImpl(Java8Parser.SingleTypeImportDeclarationContext);
             return super.visitImportDeclaration(ctx);
         }
