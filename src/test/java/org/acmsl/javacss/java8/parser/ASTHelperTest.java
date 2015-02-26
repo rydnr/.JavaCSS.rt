841a0cd5339ac249a40dac1aedf231f09567a5fe
chous <chous@acm-sl.org>
2015-02-26 16:40:17 +0100

    201502261640

--- src/test/java/org/acmsl/javacss/java8/parser/ASTHelperTest.java
+++ src/test/java/org/acmsl/javacss/java8/parser/ASTHelperTest.java
@@ -82,7 +82,7 @@ public class ASTHelperTest
         boolean found = false;
 
         for (ParseTree node : imports) {
-            if (node instanceof ImportDeclarationContext) {
+            for ()
 
                 TerminalNode leaf = (TerminalNode) node;
 
