f936c8e3156dfd0aada379e787f8dd6c70656c9e
chous <chous@acm-sl.org>
2015-02-26 15:17:25 +0100

    201502261517

--- src/test/java/org/acmsl/javacss/java8/parser/ASTHelperTest.java
+++ src/test/java/org/acmsl/javacss/java8/parser/ASTHelperTest.java
@@ -80,7 +80,7 @@ public class ASTHelperTest
         Assert.assertNotNull(imports);
         for (ParseTree node : imports) {
             if (node instanceof TerminalNode) {
-                TerminalNode
+                TerminalNode leaf =
 
             }
         }
