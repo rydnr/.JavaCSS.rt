067b5f4fe951652fe53516072c84a0b0f4c90b5f
chous <chous@acm-sl.org>
2015-02-26 15:17:39 +0100

    201502261517

--- src/test/java/org/acmsl/javacss/java8/parser/ASTHelperTest.java
+++ src/test/java/org/acmsl/javacss/java8/parser/ASTHelperTest.java
@@ -82,7 +82,7 @@ public class ASTHelperTest
             if (node instanceof TerminalNode) {
                 TerminalNode leaf = (TerminalNode) node;
 
-                if ()
+                if (leaf.getText())
 
             }
         }
