c67ea1ddce819b00621d4cda806dc38cb3471578
chous <chous@acm-sl.org>
2015-02-26 23:41:48 +0100

    201502262341

--- src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
+++ src/main/java/org/acmsl/javacss/java8/parser/ASTHelper.java
@@ -91,7 +91,6 @@ public class ASTHelper
             singleTypeImportDeclarationContext.addChild(new CommonToken(Java8Parser.IMPORT, "import"));
             TypeNameContext typeNameContext =
                 new TypeNameContext(singleTypeImportDeclarationContext, singleTypeImportDeclarationContext.invokingState);
-
             newImport.addChild(new CommonToken(Java8Parser.Identifier, importType));
             ctx.addChild(newImport);
             return super.visitCompilationUnit(ctx);
