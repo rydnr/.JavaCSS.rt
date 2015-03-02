479de24cb11328b2dd7ad14b55620f553c2fec82
chous <chous@acm-sl.org>
2015-03-02 16:55:46 +0100

    201503021655

--- src/test/java/org/acmsl/javacss/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/StringTemplateCSSHelperTest.java
@@ -58,6 +58,6 @@ public class StringTemplateCSSHelperTest
 
         StringTemplateCSSHelper helper = new StringTemplateCSSHelper(input);
 
-        helper.get
+        helper.getSelectors()
     }
 }
