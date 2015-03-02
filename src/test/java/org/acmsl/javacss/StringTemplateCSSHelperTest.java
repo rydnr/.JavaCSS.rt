b780dbc45a49e2215675ef5b014e29315559290c
chous <chous@acm-sl.org>
2015-03-02 17:09:34 +0100

    201503021709

--- src/test/java/org/acmsl/javacss/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/StringTemplateCSSHelperTest.java
@@ -67,6 +67,6 @@ public class StringTemplateCSSHelperTest
 
         Assert.assertEquals(1, selectors.size());
 
-        Assert.assertEquals(".packageDeclaration")
+        Assert.assertEquals(".packageDeclaration #identifier")
     }
 }
