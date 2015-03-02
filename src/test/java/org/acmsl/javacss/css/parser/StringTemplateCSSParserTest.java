860a9fcf171ee3a3eac19f6093686af421240cbe
chous <chous@acm-sl.org>
2015-03-02 16:09:59 +0100

    201503021609

--- src/test/java/org/acmsl/javacss/css/parser/StringTemplateCSSParserTest.java
+++ src/test/java/org/acmsl/javacss/css/parser/StringTemplateCSSParserTest.java
@@ -7,10 +7,42 @@ import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.Test;
+import org.stringtemplate.v4.STErrorListener;
+import org.stringtemplate.v4.misc.STMessage;
 
 @RunWith(JUnit4.class)
 public class StringTemplateCSSParserTest
 {
+    /**
+     * Builds an error manager to make the tests fail upon any error.
+     * @return such {@link ErrorManager}.
+     */
+    protected ErrorManager buildErrorManager() {
+        return
+            new ErrorManager(
+                new STErrorListener() {
+                    @Override
+                    public void compileTimeError(final STMessage msg) {
+                        Assert.fail(msg.toString());
+                    }
+
+                    @Override
+                    public void runTimeError(final STMessage msg) {
+                        Assert.fail(msg.toString());
+                    }
+
+                    @Override
+                    public void IOError(final STMessage msg) {
+                        Assert.fail(msg.toString());
+                    }
+
+                    @Override
+                    public void internalError(final STMessage msg) {
+                        Assert.fail(msg.toString());
+                    }
+                });
+    }
+
     @Test
     public void parses_a_simple_input() {
         String input =
