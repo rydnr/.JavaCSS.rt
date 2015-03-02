5867063ea07b20f61e8b3176afa52f91bd0ccb48
chous <chous@acm-sl.org>
2015-03-02 16:55:04 +0100

    201503021655

--- src/test/java/org/acmsl/javacss/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/StringTemplateCSSHelperTest.java
@@ -49,5 +49,7 @@ import org.junit.runners.JUnit4;
 public class StringTemplateCSSHelperTest
 {
     @Test
-    public void retrieves_properties_for_a_simple_input
+    public void retrieves_properties_for_a_simple_input(){
+
+    }
 }
