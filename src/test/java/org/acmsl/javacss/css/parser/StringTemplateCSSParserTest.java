7c3ddef0c8c7927224f14ccfe8b0b001669c286b
chous <chous@acm-sl.org>
2015-03-02 16:17:40 +0100

    201503021617

--- src/test/java/org/acmsl/javacss/css/parser/StringTemplateCSSParserTest.java
+++ src/test/java/org/acmsl/javacss/css/parser/StringTemplateCSSParserTest.java
@@ -1,32 +1,17 @@
 package org.acmsl.javacss.css.parser;
 
-import org.antlr.v4.runtime.ANTLRErrorStrategy;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.tool.ErrorManager;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.Test;
-import org.stringtemplate.v4.STErrorListener;
-import org.stringtemplate.v4.misc.STMessage;
 
 @RunWith(JUnit4.class)
 public class StringTemplateCSSParserTest
 {
-    /**
-     * Builds an error manager to make the tests fail upon any error.
-     * @return such {@link ErrorManager}.
-     */
-    protected ErrorManager buildErrorManager() {
-        return null;
-    }
-
     @Test
     public void parses_a_simple_input() {
         String input =
