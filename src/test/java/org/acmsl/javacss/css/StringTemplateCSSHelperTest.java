5a442200c337b9e0f4157ba4d57f6b8dc621b13c
chous <chous@acm-sl.org>
2015-03-03 09:55:21 +0100

    201503030955

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -97,7 +97,7 @@ public class StringTemplateCSSHelperTest
     @Test
     public void retrieves_selectors_for_an_input_with_several_blocks()
     {
-
+        multipleBlockTests();
         StringBuilder input =
             new StringBuilder(".packageDeclaration #identifier");
 
