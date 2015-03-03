a53956a2c540d5db5cc2e60ca8a18d07880bef4d
chous <chous@acm-sl.org>
2015-03-03 18:38:02 +0100

    201503031838

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -111,7 +111,7 @@ public class StringTemplateCSSHelperTest
             input.append(".packageDeclaration #identifier");
             input.append(index);
             input.append("::before {\n");
-            input.append(+ "    content: \"  \";\n"
+            input.append("    content: \"  \";\n"
             + "}\n";
 
         StringTemplateCSSHelper helper = new StringTemplateCSSHelper(input);
