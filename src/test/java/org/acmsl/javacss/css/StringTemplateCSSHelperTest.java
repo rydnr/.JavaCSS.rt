2b5e297a8539f1d06402088591f5d7d907f49c96
chous <chous@acm-sl.org>
2015-03-03 18:50:09 +0100

    201503031850

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -147,6 +147,7 @@ public class StringTemplateCSSHelperTest
     @Test
     public void retrieves_properties_for_an_input_with_multiple_blocks()
     {
-        multipleBlockPropertyTests((int) (Math.random() * 10));
+//        multipleBlockPropertyTests((int) (Math.random() * 10));
+        multipleBlockPropertyTests(2);
     }
 }
