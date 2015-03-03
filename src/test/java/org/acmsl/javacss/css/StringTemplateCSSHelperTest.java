f7ee48d168745d9ab213089119d4ba96cff07e6f
chous <chous@acm-sl.org>
2015-03-03 09:54:45 +0100

    201503030954

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -86,7 +86,7 @@ public class StringTemplateCSSHelperTest
 
         Assert.assertNotNull(selectors);
 
-        Assert.assertEquals(1, selectors.size());
+        Assert.assertEquals(count, selectors.size());
 
         Assert.assertEquals(".packageDeclaration#identifier::before", selectors.get(0));
     }
