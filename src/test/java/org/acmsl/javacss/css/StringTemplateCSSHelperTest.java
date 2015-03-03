62709743fedebbd8b180a411e49f389ce624139f
chous <chous@acm-sl.org>
2015-03-03 09:53:16 +0100

    201503030953

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -78,8 +78,7 @@ public class StringTemplateCSSHelperTest
 
         for (int index = 0; index < 10; index++)
         {
-            input.append()::
-        }before {\n"
+            input.append("::before {\n"
             + "    content: \"  \";\n"
             + "}\n";
 
