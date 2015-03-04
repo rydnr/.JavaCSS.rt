772541abe4badf54e18c5fc3638ea86924c2e6f7
chous <chous@acm-sl.org>
2015-03-04 18:30:18 +0100

    201503041830

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -186,6 +186,7 @@ public class StringTemplateCSSHelperTest
         String matchedSelectors = helper.retrieveMatchingSelectors(semiColon);
 
         Assert.assertNotNull(matchedSelectors);
+        Assert.assertEquals()
     }
 }
 
