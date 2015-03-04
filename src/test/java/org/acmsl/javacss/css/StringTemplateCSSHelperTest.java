de69a4d734ac00a69cf03079f089dfd993d94a69
chous <chous@acm-sl.org>
2015-03-04 18:29:20 +0100

    201503041829

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -183,7 +183,7 @@ public class StringTemplateCSSHelperTest
         ParseTree semiColon = matches.toArray(new ParseTree[1])[0];
         Assert.assertNotNull(semiColon);
 
-        Assert.assertTrue(helper)
+        Assert.assertTrue()
     }
 }
 
