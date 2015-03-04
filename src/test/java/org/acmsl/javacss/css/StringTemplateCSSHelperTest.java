e3e2997ff66a8504c4fcb6e8083da61acf8a197f
chous <chous@acm-sl.org>
2015-03-04 20:42:23 +0100

    201503042042

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -76,7 +76,7 @@ public class StringTemplateCSSHelperTest
         Assert.assertEquals(1, selectorCombinations.size());
 
         List<String> selectors = selectorCombinations.get(0);
-        Assert.assertEquals(2, selectorCombinations.size());
+        Assert.assertEquals(2, .size());
 
         Assert.assertEquals(".packageDeclaration", selectors.get(0));
         Assert.assertEquals("#identifier::before", selectors.get(1));
