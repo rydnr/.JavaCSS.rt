f41677b9982a502639211cee6f500fbb1f9f96e1
chous <chous@acm-sl.org>
2015-03-04 20:36:33 +0100

    201503042036

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -105,7 +105,7 @@ public class StringTemplateCSSHelper
 
         for (ParseTree selectorCombination : selectorCombinations)
         {
-            List<String> selector
+            List<String> selectors = new ArrayList<>
         }
             for (ParseTree selector: selectorCombination)
             {
