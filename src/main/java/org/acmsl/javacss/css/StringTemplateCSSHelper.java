72037634c1c91deab46f6b036d789f9f4e78b12f
chous <chous@acm-sl.org>
2015-03-04 20:37:25 +0100

    201503042037

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -111,7 +111,7 @@ public class StringTemplateCSSHelper
             for (ParseTree selector: selectorCombination)
             {
                 String text = selector.getText();
-                currentSelectors
+                currentSelectors.add(text)
             Map<String, String> block = retrieveProperties(selectorCombination, parser);
 
             this.properties.put(text, block);
