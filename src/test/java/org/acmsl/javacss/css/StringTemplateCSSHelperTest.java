12966b0ea1241ea46ba2bb48fc346114d120f36c
chous <chous@acm-sl.org>
2015-03-04 20:40:47 +0100

    201503042040

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -75,7 +75,7 @@ public class StringTemplateCSSHelperTest
 
         Assert.assertEquals(1, selectors.size());
 
-        Assert.assertEquals(".packageDeclaration", "selectors.get(0).getText());
+        Assert.assertEquals(".packageDeclaration", selectors.get(0).getText());
                             Assert.assertEquals(".packageDeclaration", #identifier::before", " +
                                                                                           "selectors.get(0).getText());
     }
