f0c67ad5e824a4ed73cb8bcdfedd362cc8f2cced
chous <chous@acm-sl.org>
2015-03-19 21:05:44 +0100

    201503192105

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -255,7 +255,7 @@ public class StringTemplateCSSHelper
                 // class selector
                 result = node.getPayload().getClass().getSimpleName().equals(currentSelector.substring(1));
             } else if (currentSelector.startsWith("\"")) {
-                result = node.getPayload().toString()
+                result = node.getPayload().toString().equals()
             }
             return result;
         }
