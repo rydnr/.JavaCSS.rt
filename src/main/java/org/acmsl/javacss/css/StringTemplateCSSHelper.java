d2ee2ad26541b28bfae1c7d024a7f9a508ac436e
chous <chous@acm-sl.org>
2015-03-19 20:15:34 +0100

    201503192015

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -210,7 +210,7 @@ public class StringTemplateCSSHelper
         boolean match = false;
 
         List<String> selectors;
-        Stack<String> matchin
+        Stack<String>
         public SelectorMatchVisitor(List<String> selectors) {
             this.selectors = selectors;
         }
