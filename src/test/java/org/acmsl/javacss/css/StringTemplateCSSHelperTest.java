d2da3380e58cd582cd96e34bd5f9b8078badf71a
chous <chous@acm-sl.org>
2015-03-19 19:42:51 +0100

    201503191942

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -188,7 +188,7 @@ public class StringTemplateCSSHelperTest
         ParseTree semiColon = matches.toArray(new ParseTree[1])[0];
         Assert.assertNotNull(semiColon);
 
-        List<String> matchedSelectors = helper.retrieveMatchingSelectors(semiColon, ast);
+        List<String> matchedSelectors = helper.match()
 
         Assert.assertNotNull(matchedSelectors);
         Assert.assertEquals(2, matchedSelectors.size());
