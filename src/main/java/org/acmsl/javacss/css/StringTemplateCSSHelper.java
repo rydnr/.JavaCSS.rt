0680bfe9b31e5a2254b70723d61b4ecad3530896
chous <chous@acm-sl.org>
2015-03-19 20:16:42 +0100

    201503192016

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -210,13 +210,14 @@ public class StringTemplateCSSHelper
     {
         boolean match = false;
 
-        List<String> selectors;
-        Iterator<String> iterator;
+        final List<String> selectors;
+        final Iterator<String> iterator;
 
         Stack<String> selectorsInProcess = new Stack<String>();
 
         public SelectorMatchVisitor(List<String> selectors) {
             this.selectors = selectors;
+            this.iterator = selectors.iterator();
         }
 
         @Override
