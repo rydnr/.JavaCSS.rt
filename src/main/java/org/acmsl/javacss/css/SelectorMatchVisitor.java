e7466ab46696d94b25d2f2bc07a1d2a633f09a1f
chous <chous@acm-sl.org>
2015-03-20 10:36:02 +0100

    201503201036

--- src/main/java/org/acmsl/javacss/css/SelectorMatchVisitor.java
+++ src/main/java/org/acmsl/javacss/css/SelectorMatchVisitor.java
@@ -52,7 +52,7 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * An ANTLR visitor to find whether there're s
+ * An ANTLR visitor to find whether some selectors match
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2015/03/20 10:35
