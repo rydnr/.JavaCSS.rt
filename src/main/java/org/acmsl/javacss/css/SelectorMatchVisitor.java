e7da0df2d05650a4aa39e47774cbc0e1b6c7a8c2
chous <chous@acm-sl.org>
2015-03-20 11:12:39 +0100

    201503201112

--- src/main/java/org/acmsl/javacss/css/SelectorMatchVisitor.java
+++ src/main/java/org/acmsl/javacss/css/SelectorMatchVisitor.java
@@ -113,7 +113,6 @@ public class SelectorMatchVisitor
                     className = className.substring(className.lastIndexOf("$"));
                 }
                 result = currentSelector.equals("." + className.substring(0, className.lastIndexOf("Context")));
-                result = node.getPayload().getClass().getSimpleName().equals(currentSelector.substring(1));
             } else if (currentSelector.startsWith("\"")) {
                 result = node.getPayload().toString().equals(currentSelector.substring(1, currentSelector.lastIndexOf("\"")));
             }
