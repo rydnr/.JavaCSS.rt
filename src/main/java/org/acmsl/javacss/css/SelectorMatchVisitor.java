e929d67233f1fbbf74ab2e64084180079ef3aede
chous <chous@acm-sl.org>
2015-03-20 11:20:41 +0100

    201503201120

--- src/main/java/org/acmsl/javacss/css/SelectorMatchVisitor.java
+++ src/main/java/org/acmsl/javacss/css/SelectorMatchVisitor.java
@@ -123,7 +123,7 @@ public class SelectorMatchVisitor
 
                 result = currentSelector.equals("." + className);
             } else if (currentSelector.startsWith("\"")) {
-                Object
+                Object payload =
                 String value =
                 result = node.getPayload().toString().equals(currentSelector.substring(1, currentSelector.lastIndexOf("\"")));
             }
