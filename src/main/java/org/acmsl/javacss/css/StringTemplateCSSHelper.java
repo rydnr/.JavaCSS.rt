372132000545e5bd31df7ee49a785a5d35a01af7
chous <chous@acm-sl.org>
2015-03-26 13:11:56 +0100

    201503261311

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -166,7 +166,7 @@ public class StringTemplateCSSHelper
                     css.addSelector(selector);
                 }
                 Map<String, String> properties = this.properties.get(selectors);
-                css.
+                for (Map.Entry<String, String> entry : this.properties.get(selectors))
                 result.add(css);
                 break;
             }
