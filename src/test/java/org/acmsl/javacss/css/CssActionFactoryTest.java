4f6344c4ccd3a0a9ee673ca835ef28fd96bf35d1
chous <chous@acm-sl.org>
2015-03-26 19:20:10 +0100

    201503261920

--- src/test/java/org/acmsl/javacss/css/CssActionFactoryTest.java
+++ src/test/java/org/acmsl/javacss/css/CssActionFactoryTest.java
@@ -58,5 +58,5 @@ import org.junit.runners.JUnit4;
 public class CssActionFactoryTest
 {
     @Test
-    public void when_there_is_nothing_to_do_returns_Null
+    public void when_there_is_nothing_to_do_returns_NullCssAction
 }
