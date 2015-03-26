01ff13ae9440b44434ef4d8ea616ddb5a64a4b8d
chous <chous@acm-sl.org>
2015-03-26 16:39:00 +0100

    201503261639

--- src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
+++ src/main/java/org/acmsl/javacss/css/StringTemplateCSSHelper.java
@@ -57,10 +57,8 @@ import org.checkthread.annotations.ThreadSafe;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 
 /**
  *
