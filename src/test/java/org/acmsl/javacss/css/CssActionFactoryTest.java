5b6cfa129ef2f32cd60d742b457b9cfed0141976
chous <chous@acm-sl.org>
2015-03-26 20:46:44 +0100

    201503262046

--- src/test/java/org/acmsl/javacss/css/CssActionFactoryTest.java
+++ src/test/java/org/acmsl/javacss/css/CssActionFactoryTest.java
@@ -60,7 +60,7 @@ public class CssActionFactoryTest
 {
     @Test
     public void when_there_is_nothing_to_do_createAction_returns_NullCssAction() {
-        CssActionFactory factory = new CssActionFactory();
+        CssActionFactory factory = new CssActionFacta{ory();
 
         Css css = new Css();
         css.addSelector(".rule");
