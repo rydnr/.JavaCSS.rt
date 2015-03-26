244554dfcfbdb7b83a19301e4e37c8d3402a7154
chous <chous@acm-sl.org>
2015-03-26 12:15:42 +0100

    201503261215

--- src/main/java/org/acmsl/javacss/css/Css.java
+++ src/main/java/org/acmsl/javacss/css/Css.java
@@ -1,7 +1,7 @@
 /*
-                        devdel
+                        JavaCSS
 
-    Copyright (C) 2002-today  Jose San Leandro Armendariz
+    Copyright (C) 2015-today  Jose San Leandro Armendariz
                               chous@acm-sl.org
 
     This library is free software; you can redistribute it and/or
@@ -27,7 +27,7 @@
  *
  * Author: Jose San Leandro Armendariz
  *
- * Description: 
+ * Description: Represents CSS blocks.
  *
  * Date: 2015/03/26
  * Time: 12:12
