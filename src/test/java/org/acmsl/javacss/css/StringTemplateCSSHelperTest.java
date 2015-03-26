78250fb6de119dfc342067c270beb5547e98501a
chous <chous@acm-sl.org>
2015-03-26 12:12:26 +0100

    201503261212

--- src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
+++ src/test/java/org/acmsl/javacss/css/StringTemplateCSSHelperTest.java
@@ -204,7 +204,7 @@ public class StringTemplateCSSHelperTest
     }
 
     @Test
-    public void finds_the_matching_selector() {
+    public void finds_the_matching_css() {
         String javaInput = "package com.foo.bar;";
 
         String cssInput =
