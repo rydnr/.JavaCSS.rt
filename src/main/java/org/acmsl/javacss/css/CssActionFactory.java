e520672ead5cb34faa2431eef5009175f3351bb0
chous <chous@acm-sl.org>
2015-03-27 08:17:10 +0100

    201503270817

--- src/main/java/org/acmsl/javacss/css/CssActionFactory.java
+++ src/main/java/org/acmsl/javacss/css/CssActionFactory.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Crea
+ * Creates {@link CssAction} instances.
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * Created: 2015/03/27 08:16
  */
