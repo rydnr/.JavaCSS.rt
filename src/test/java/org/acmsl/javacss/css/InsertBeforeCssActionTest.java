48c7fe5231de391e35d822e6a09e0252dd2e95a7
chous <chous@acm-sl.org>
2015-03-27 18:10:26 +0100

    201503271810

--- src/test/java/org/acmsl/javacss/css/InsertBeforeCssActionTest.java
+++ src/test/java/org/acmsl/javacss/css/InsertBeforeCssActionTest.java
@@ -61,6 +61,6 @@ public class InsertBeforeCssActionTest {
         Css css = new Css();
 
         css.addSelector(".rule::before");
-        css.addProperty("content", "css-prefix");
+        css.addProperty(new Property("content", "css-prefix"));
     }
 }
