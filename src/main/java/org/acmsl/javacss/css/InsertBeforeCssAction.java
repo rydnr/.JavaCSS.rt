0966b8b620f971b5643667ead82b7774a3ee0217
chous <chous@acm-sl.org>
2015-03-27 16:54:07 +0100

    201503271654

--- src/main/java/org/acmsl/javacss/css/InsertBeforeCssAction.java
+++ src/main/java/org/acmsl/javacss/css/InsertBeforeCssAction.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- *
+ * A {@link CssAction} th
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * Created: 2015/03/27 16:53
  */
