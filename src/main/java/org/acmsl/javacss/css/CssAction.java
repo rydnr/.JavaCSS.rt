914b049298996d11ddbad436c14a1fd6b7ef5e25
chous <chous@acm-sl.org>
2015-03-27 08:20:36 +0100

    201503270820

--- src/main/java/org/acmsl/javacss/css/CssAction.java
+++ src/main/java/org/acmsl/javacss/css/CssAction.java
@@ -46,7 +46,7 @@ import org.jetbrains.annotations.NotNull;
 import org.checkthread.annotations.ThreadSafe;
 
 /**
- * Represents all actions to be performed after
+ * Represents all actions to be performed according to CSS bl
  * @author <a href="mailto:queryj@acm-sl.org">Jose San Leandro</a>
  * @since 3.0
  * Created: 2015/03/27 08:19
